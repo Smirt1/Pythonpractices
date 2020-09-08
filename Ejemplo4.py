01b43b7dc8299e08c233c355af84f52950a3f55b
Smirt Valentin <smirtvs@gmail.com>
2020-09-08 12:40:42 -0400

    tabla de multiplicar

--- Ejemplo4.py
+++ Ejemplo4.py
@@ -8,10 +8,20 @@
 # print ("El bucle ha finalizado")
 
 
-c = 1
-while c <= 10:
-    if c % 2 == 0:
-        print (c)
-    c = c + 1
+# c = 1
+# while c <= 10:
+#     if c % 2 == 0:
+#         print (c)
+#     c = c + 1
 
-print ("El bucle ha finalizado")
+# print ("El bucle ha finalizado")
+
+# tabla de multiplicar
+
+n = input("ingrese un numero para ver la tabla")
+c = 1
+
+while c <= 12:
+    print(int (n)* c )
+    c += 1 # c = c + 1
+    
