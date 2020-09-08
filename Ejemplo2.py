15c3dfad7f6223e4c20f471c79141d8eb934d5dc
Smirt Valentin <smirtvs@gmail.com>
2020-09-08 09:14:36 -0400

    Mas ejemplos

--- Ejemplo2.py
+++ Ejemplo2.py
@@ -1,3 +1,41 @@
-#Condicional
-numero = 14
-if numero % 2 == 0
+# # Condicional
+# numero = 14
+# if numero % 2 == 0:
+#     print("Par")
+
+#     print("gracias por usar mi programa")
+
+# edad = input("Ingrese su edad:")
+# edad = int(edad)
+# if edad >= 18:
+#     print ("Usted es mayor de edad")
+#     print("Usted puede beber")
+
+# Login
+
+# usuario = input("Ingrese su usuario:")
+# if usuario == 'admin':
+#     print("Acceso concedido")
+# else:
+#     print("Usuario no valido")
+
+# if.. elif .. else
+
+dia = 6
+
+if dia == 1:
+    print ("Lunes")
+elif dia == 2:
+    print ("Martes")
+elif dia == 3:
+    print ("Miercoles")
+elif dia == 4:
+    print ("Jueves")
+elif dia == 5:
+    print ("Viernes")
+else:
+    print ("Es fin de semana")
+
+    
+
+
