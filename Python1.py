aea1b129844558075eed8859953aa9eb15dd5192
Smirt Valentin <smirtvs@gmail.com>
2020-09-08 08:40:09 -0400

    Ejemplos del viernes pasado

--- Python1.py
+++ Python1.py
@@ -1,1 +1,23 @@
-print ("otherget")
+# Esto es un comentario
+
+
+# Hacer salida de datos en python
+# print("Hello")
+#print (2 + 4 + 6)
+
+# Entrada de datos
+# print ("Escriba su edad: ")
+# edad = input ()
+# print ("La edad es: " + edad)
+
+#Almacenamiento en memoria de los datos
+# numero = 110
+# print (numero + 5)
+
+# Expresiones
+precio = 99.99
+cantidad = 5
+total = cantidad * precio
+print (total)
+resultado = 1 < 2
+print (resultado)
