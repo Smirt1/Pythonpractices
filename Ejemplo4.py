a1459ddd468f5d702dcf1e599ef046ad48d62ea9
Smirt Valentin <smirtvs@gmail.com>
2020-09-08 12:34:59 -0400

    Cambio

--- Ejemplo4.py
+++ Ejemplo4.py
@@ -1,8 +1,17 @@
 # While (mientras)
 
+# c = 1
+# while c <= 10:
+#     print (c)
+#     c = c + 1
+
+# print ("El bucle ha finalizado")
+
+
 c = 1
 while c <= 10:
-    print (c)
+    if c % 2 == 0:
+        print (c)
     c = c + 1
 
 print ("El bucle ha finalizado")
