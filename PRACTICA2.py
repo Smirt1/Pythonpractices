02943deaeee097e7627f7aa3b7d29e884cbc2084
Smirt Valentin <smirtvs@gmail.com>
2020-09-09 23:35:36 -0400

    cambio

--- PRACTICA2.py
+++ PRACTICA2.py
@@ -1,11 +1,12 @@
 # Sumatoria
 
-suma = 0
+suma = 1
 numero = int(input("Ingrese un numero: "))
 
 while numero >= 0:
     suma += numero
     numero = int(input("Ingrese otro un numero: "))
 
-    print(suma)
+    
+    print(f"La suma de los numeros introducidos es {suma}.")
 
