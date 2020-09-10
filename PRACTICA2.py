cbd181ec8f605c7057f84095e26483e1d7b33323
Smirt Valentin <smirtvs@gmail.com>
2020-09-09 23:11:16 -0400

    Sumatoria de la practica 2 lista

---
+++ PRACTICA2.py
@@ -0,0 +1,11 @@
+# Sumatoria
+
+suma = 0
+numero = int(input("Ingrese un numero: "))
+
+while numero >= 0:
+    suma += numero
+    numero = int(input("Ingrese otro un numero: "))
+
+    print(suma)
+
