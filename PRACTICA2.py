faec083bdb19703ef20eef21a09af29984e947be
Smirt Valentin <smirtvs@gmail.com>
2020-09-10 15:10:05 -0400

    Modificaciones

--- PRACTICA2.py
+++ PRACTICA2.py
@@ -13,30 +13,28 @@
 
 #Realizar un programa que presente un menú con las siguientes opciones: 
 
-# Imprimimos el menú en pantalla
+
 print("""
     1) Convertir grados a Celsius a Fahrenheit       3) Convertir dólar a pesos
     2) Convertir metros a pies      4) Salir
     """)
 
-# Leemos lo que ingresa el usuario
-eligio=input("-Selecciona algo :")
+opcion=input("-Selecciona algo :")
 
-# Según lo que ingresó, código diferente
-if eligio=="1":    
-    gradoscel = int(input ( "Intruzca la temperatura a convertir: "))
+if opcion=="1":    
+    gradoscel = int(input ( "Introduzca la temperatura a convertir: "))
     formula = 9 / 5 * gradoscel + 32
     print (formula)
-elif eligio=="2":
-    x = 3
-    y = 5
-    print("x * y = ", x * y)
-elif eligio=="3":
-    cantidad = float(input ( "Escriba la contidad de efectivo a convertir: "))
+elif opcion=="2":
+    metros = float(input ( "Intruzca la cantidad de metros a convertir: "))
+    formula = 3.280840 * metros
+    print(formula)
+elif opcion=="3":
+    cantidad = float(input ( "Escriba la cantidad de efectivo a convertir: "))
     tasa = 60
     operacion = (cantidad * tasa)
     print( operacion)
-elif eligio=="4":
+elif opcion=="4":
     print("Gracias por usar mi programa")
 else:
     print("Opción no válida")
---
+++ Prueba.py
@@ -0,0 +1,5 @@
+#Convierta grados celsius a Fahrenheit
+
+metros = float(input ( "Intruzca la cantidad de metros a convertir: "))
+formula = 3.280840 * metros
+print(formula)
